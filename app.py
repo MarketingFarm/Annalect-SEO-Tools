657e60b3f5e9b758f52aff21a43566c3b1daaa06
MarketingFarm <info@marketingfarm.it>
2025-05-08 14:32:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.set_page_config(
 # Sidebar: logo
 st.sidebar.markdown(
     '<div style="text-align:left; margin-bottom:20px;">'
-    '<img src="https://i.ibb.co/C57SRppY/annalect-logo-400x113.png" width="180"/>'
+    '<img src="https://i.ibb.co/C57SRppY/annalect-logo-400x113.png" width="150"/>'
     '</div>',
     unsafe_allow_html=True
 )
