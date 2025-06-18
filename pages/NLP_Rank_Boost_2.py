23df8e71ce14a81d38fc32ba334b1ad7bf6f34f9
MarketingFarm <info@marketingfarm.it>
2025-06-18 17:42:32 +0200

    Update NLP_Rank_Boost_2.py

--- pages/NLP_Rank_Boost_2.py
+++ pages/NLP_Rank_Boost_2.py
@@ -119,7 +119,7 @@ if st.session_state.step == 1:
   background-color: rgb(247 248 249);
   margin-bottom: 0.5rem;
 ">
-  <div style="font-size:0.8rem; color: rgb(255 70 70);">{label}</div>
+  <div style="font-size:0.8rem; color: rgb(143 143 143);">{label}</div>
   <div style="font-size:1.1rem; color:#202124; font-weight:500; line-height: 1.4rem;">{val}</div>
 </div>
         """, unsafe_allow_html=True)
@@ -146,7 +146,7 @@ if st.session_state.step == 1:
   border-radius: 0.5rem;
   background-color: rgb(247 248 249);
 ">
-  <div style="font-size:0.8rem; color: rgb(255 70 70);">{lbl}</div>
+  <div style="font-size:0.8rem; color: rgb(143 143 143);">{lbl}</div>
   <div style="font-size:1.1rem; color:#202124; font-weight:500; line-height: 1.4rem;">{v}</div>
 </div>
         """, unsafe_allow_html=True)
