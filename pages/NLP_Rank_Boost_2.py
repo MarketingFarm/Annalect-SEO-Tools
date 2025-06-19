7407e2598399b2d1aa765d6814f188f6debaad5a
MarketingFarm <info@marketingfarm.it>
2025-06-19 10:44:08 +0200

    Update NLP_Rank_Boost_2.py

--- pages/NLP_Rank_Boost_2.py
+++ pages/NLP_Rank_Boost_2.py
@@ -312,7 +312,7 @@ elif st.session_state.step == 4:
     with col1:
         context = st.selectbox(
             "Contesto",
-            ["-- Seleziona --", "E-commerce", "Blog / Testata Giornalistica"],
+            ["-- Seleziona --", "E-commerce", "Magazine / Testata Giornalistica"],
             key="context_select"
         )
     with col2:
@@ -326,7 +326,7 @@ elif st.session_state.step == 4:
                 "Pagina Informativa",
                 "Articolo del Blog"
             ],
-            "Blog / Testata Giornalistica": ["-- Seleziona --", "Articolo del Blog"]
+            "Magazine / Testata Giornalistica": ["-- Seleziona --", "Articolo del Blog"]
         }
         opts = dest_options.get(context, ["-- Seleziona --"])
         destino = st.selectbox(
